70ef80d0f5f33348fc4787c9cb2749e11bc3667d
jyh <jyh9177@naver.com>
2023-04-13 21:42:21 +0900

    1

---
+++ venv/Lib/이코테/정렬/스와프 practice.py
@@ -0,0 +1,14 @@
+array = [3, 5]
+array[0], array[1] = array[1], array[0]
+
+print(array)
+
+array1 = [3, 5]
+array1[0], array1[1] = array1[1], array1[0]
+
+print(array1)
+
+array2 = [3, 5]
+array2[0], array2[1] = array2[1] , array2[0]
+
+print(array2)
