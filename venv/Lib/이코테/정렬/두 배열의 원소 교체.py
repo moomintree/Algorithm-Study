5fef680175fdaf7b01b31a201cd09ed4eb4c3257
jyh <jyh9177@naver.com>
2023-04-14 20:21:28 +0900

    1

---
+++ venv/Lib/이코테/정렬/두 배열의 원소 교체.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+for i in range(k):
+
+    # A의 원소가 B의 원소보다 작은 경우
+    if a[i] < b[i]:
+        # 두 원소를 교체
+        a[i], b[i] = b[i], a[i]
+    else:  # A의 원소가 B의 원소보다 크거나 같을 때, 반복문을 탈출
+        break
+
+print(sum(a)) # 배열 A의 모든 원소의 합을 출력
